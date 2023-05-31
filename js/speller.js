cf5c9bbde2a72107a50550e87672aefe796982fc
Cameron Dubas <camerondubas@gmail.com>
2023-05-31 16:02:48 +0300

    wip

--- js/speller.js
+++ js/speller.js
@@ -1,30 +1,67 @@
 export default {
-	check,
-	lookup,
+  check,
+  lookup,
 };
 
 var elements;
+const symbols = {};
 
 await loadPeriodicTable();
-
 
 // ****************************
 
 async function loadPeriodicTable() {
-	elements = await (await fetch("periodic-table.json")).json();
+  elements = await (await fetch("periodic-table.json")).json();
+
+  for (let element of elements) {
+    symbols[element.symbol.toLowerCase()] = element;
+  }
+
+  return {};
 }
 
-function check(inputWord) {
-	// TODO: determine if `inputWord` can be spelled
-	// with periodic table symbols; return array with
-	// them if so (empty array otherwise)
+function check(charsLeft) {
+  if (charsLeft.length == 0) {
+    return [];
+  }
 
-	return [];
+  // check 2 letter symbols first
+  if (charsLeft.length >= 2) {
+    const two = charsLeft.slice(0, 2);
+    const rest = charsLeft.slice(2);
+
+    if (two in symbols) {
+      if (rest !== "") {
+        let result = [two, ...check(rest)];
+        if (result.join("") == charsLeft) {
+          return result;
+        }
+      } else {
+        return [two];
+      }
+    }
+  }
+
+  // check 1 letter symbols
+  if (charsLeft.length >= 1) {
+    const one = charsLeft.slice(0, 1);
+    const rest = charsLeft.slice(1);
+
+    if (one in symbols) {
+      if (rest !== "") {
+        let result = [one, ...check(rest)];
+        if (result.join("") == charsLeft) {
+          return result;
+        }
+      } else {
+        return [one];
+      }
+    }
+  }
+
+  return [];
 }
 
 function lookup(elementSymbol) {
-	// TODO: return the element entry based on specified
-	// symbol (case-insensitive)
-
-	return {};
+  return symbols[elementSymbol];
 }
